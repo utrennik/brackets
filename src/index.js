f85208ed595fda5087ab881f4a2acdf650059e8c
Utrennik <utrennik@list.ru>
2021-01-27 23:23:56 +0300

    feat: check function implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+	let length = str.length;
+
+	for (let i = 0; i < length / 2; i++) {
+		removePair();
+		if (str.length == 0) return true;
+	}
+
+	return false;
+
+	function removePair() {
+		for (let i = 0; i < str.length; i++) {
+
+			for (let j = 0; j < bracketsConfig.length; j++) {
+
+				if (str.length == 0) {
+						return;
+				}
+
+				if ((str[i] == bracketsConfig[j][0]) && (str[i + 1] == bracketsConfig[j][1])) {
+					if (str.length == 2) {
+						str = '';
+						return;
+					}
+					
+					str = str.slice(0, i) + str.slice(i + 2);
+				}
+			}
+		}
+	}
+};
